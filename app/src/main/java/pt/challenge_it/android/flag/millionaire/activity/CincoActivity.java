6d1c0c675cfdb2a37cc6145d84051397e6aa113a
carolina <carolina0luis@gmail.com>
2015-05-10 22:59:39 +0100

    3.1

---
+++ app/src/main/java/pt/challenge_it/android/flag/millionaire/activity/CincoActivity.java
@@ -0,0 +1,60 @@
+package pt.challenge_it.android.flag.millionaire.activity;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import pt.challenge_it.android.flag.millionaire.R;
+
+public class CincoActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cinco);
+
+        TextView txtCinco = (TextView)findViewById(R.id.txtCinco);
+        txtCinco.setText("A resposta " + .getText());
+
+        Button btnOk = (Button)findViewById(R.id.btnOk);
+
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    finish();
+            }
+        });
+
+
+
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_cinco, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
